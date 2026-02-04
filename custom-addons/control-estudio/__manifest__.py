12c000959b54a8df555b71f6bee118244c801bba
joseville07 <jose07villegas@gmail.com>
2026-02-04 09:48:38 -0400

    modelo MVC

---
+++ custom-addons/control-estudio/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Estudiantes',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        ''
+    ],
+    'auto_install': False,
+    'application': True
+    
+}
