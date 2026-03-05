6fe6f0b25d9add1ea6f741ecfcfa00aac2bfa1db
alegutierrez <alejandro03092005@outlook.com>
2026-03-05 13:04:15 -0400

    Organizacion de carpetas

--- custom-addons/registration_module/__manifest__.py
+++ custom-addons/registration_module/__manifest__.py
@@ -3,18 +3,22 @@
     'version': '19.0.0.1',
     'description': 'Registration module for Odoo from study case',
     'summary': 'Registration module for Odoo from study case',
-    'author': '5to Semestre PR25-3',
+    'author': '5to Semestre PR25-3',    
     'website': 'None',
     'license': 'LGPL-3',
-    'category': 'Others',
+    'category': 'Education',
     'depends': [
         'base',
         'contacts'
     ],
     "data": [
         "security/ir.model.access.csv",
-        "views/registration_inscription_views.xml",
-        "views/registration_menus.xml"
+        "views/backend/registration_inscription_views.xml",
+        "views/backend/registration_menus.xml",
+        "views/frontend/templates.xml"
+        "views/frontend/pages/landing_page.xml",
+        "views/frontend/pages/success_page.xml",
+        'views/frontend/pages/enrollment_form.xml'
     ],
     'installable': True,
     'auto_install': False,
